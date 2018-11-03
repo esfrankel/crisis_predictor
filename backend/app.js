0412adcc8187af35847fedd6b9da0e4f916c11a2
Eric Frankel <eric-frankel@cox.net>
2018-11-02 23:10:38 -0700

    random shit

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
