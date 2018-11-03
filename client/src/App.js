eb04691e2137540e359c3075e03e828dd0e410ac
Michelle Bao <michellebao@Michelles-MacBook-Pro-4.local>
2018-11-03 16:45:31 -0700

    with country list

--- client/src/App.js
+++ client/src/App.js
@@ -24,33 +24,39 @@ class App extends React.Component {
         countries: [{
           id: 1,
           name: "Usa",
-          url: us
+          url: us,
+          economicScale: 5
         }, 
         {
           id: 2,
           name: "Italy",
-          url: it
+          url: it,
+          economicScale: 5
         }, 
       {
         id: 3,
         name: "Korea",
-        url: kr
+        url: kr,
+        economicScale: 7
 
       }],
-      filtered : [{
+      filtered :[{
         id: 1,
-        name: "USA",
-        url: us
+        name: "Usa",
+        url: us,
+        economicScale: 5
       }, 
       {
         id: 2,
         name: "Italy",
-        url: it
+        url: it,
+        economicScale: 5
       }, 
     {
       id: 3,
       name: "Korea",
-      url: kr
+      url: kr,
+      economicScale: 7
 
     }]
     }
@@ -59,51 +65,14 @@ class App extends React.Component {
 
 }
 
-  handleTermChange(term) {
-    // var first = term.charAt(0).toUpperCase();
-    // var res = term.substr(1).toLowerCase();
-    // var term = first + res;
-  //  onChange = (event) => {
-  //    const s = event.target.value
-  //    console.log(s);
-      
+  handleTermChange(term) {  
       const filtered = this.state.countries.filter((country) => country.name.includes(term.charAt(0).toUpperCase() + term.substr(1).toLowerCase()))
       console.log(term.charAt(0).toUpperCase() + term.substr(1).toLowerCase())
       this.setState({
           filtered: filtered
-      })
-  //}
-
-    // change this to be essentially the select 
-
-    // const url = `https://www.google.com/maps/embed/v1/place?key=AIzaSyA_Eq2me4Sgq1Ky03MDCWbA7YpKbshfm4E&q={term.replace(//g, '+')}`;
-    // request.get(url, function(err, res) {
-    //     console.log(res.body.data[0]);
-    // });  
+      }) 
   }
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     curr_item: "",
-  //     items: []
-  //   };
-  //   this.handleItem = this.handleItem.bind(this);
-  //   this.addItem = this.addItem.bind(this);
-  // }
-  // handleItem(event) {
-  //   this.setState({
-  //     curr_item: event.target.value
-  //   });
-  // }
-  // addItem() {
-  //   if(this.state.curr_item !== ''){
-  //     this.setState({
-  //       curr_item: "",
-  //       items: this.state.items.concat(this.state.curr_item)
-  //     });
-  //   }
-  // }
   render() {
     return (
       <div className="app">
