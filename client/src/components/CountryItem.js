f27432df082a676b5256ae087564f853033b5b3e
Michelle Bao <michellebao@Michelles-MacBook-Pro-4.local>
2018-11-04 00:07:45 -0700

    YUHHHS

--- client/src/components/CountryItem.js
+++ client/src/components/CountryItem.js
@@ -2,14 +2,23 @@ import React from 'react';
 import "../App.css";
 
 const CountryItem = ({ country }) => {
-  console.log(country);
+  //console.log(country);
+  let PNG;
+  try {
+    PNG = require('./Images/' + country.Alpha2.toLowerCase() + '.png');
+  }
+  catch(e) {
+
+  }
   
   return (
 
       <a href="https://www.google.com">
       <div className="country-item">
-      <img className="icon" src={country.url} alt={"meaningful description"}/>
-      <div className="countryCaption"> { country.name.toUpperCase() } </div>
+      <img className="icon" src={PNG} alt={"meaningful description"}/>
+      <div className="countryCaption"> { country.Country.toUpperCase() } </div>
+      <div className="countryMetrics"> Economic Scale: { country.AvgDiffScore} </div>
+
     </div>
     </a>
   )
--- client/src/components/CountryList.js
+++ client/src/components/CountryList.js
@@ -2,12 +2,27 @@ import React from 'react';
 import CountryItem from './CountryItem';
 
 const CountryList = (props) => {
-  const countryItems = props.countries.map((country) => {
-    return <CountryItem key={country.id} country={country} />
+  const countryItems = {};
+
+  for (var i = 0; i < props.countries.length; i++ ) {
+    var country = props.countries[i];
+
+    if (countryItems[country.Country]) {
+      if (country.Year > countryItems[country.Country].Year) {
+        countryItems[country.Country] = country;
+      }
+    }
+    else {
+      countryItems[country.Country] = country;
+    }
+  }
+
+  var renderedItems = Object.values(countryItems).map((country) => {
+    return <CountryItem key={country.Id} country={country} />
   });
 
   return (
-    <div className="country-list">{countryItems}</div>
+    <div className="country-list">{renderedItems}</div>
   );
 };
 
